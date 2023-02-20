fd4e38e24e2feea65e74a02cff6e649330dc0c62
IrinaChizhova <iriska.verbnaya@gmail.com>
2023-02-20 20:31:32 +0300

    Changed code

--- Index.test.js
+++ Index.test.js
@@ -2,13 +2,13 @@ const calculateBonus = require('./Index');
 describe('Тестирование функции calculateBonus', () => {
   it('Расчет бонусов и суммы', () => {
     expect(calculateBonus(25, 25)).toBe(50);
-    expect(calculateBonus(26, 25)).toBe(51);
-    //  expect(calculateBonus(26, 25)).toEqual(51);
+    expect(calculateBonus(26, 25)).toBe(50);
+    expect(calculateBonus(26, 25)).toEqual(51);
     expect(calculateBonus(24, 25)).toBe(49);
-    // expect(calculateBonus(10, 25)).toEqual(35);
+    expect(calculateBonus(10, 25)).toEqual(50);
   });
   it('Проверка рассчета на значение больше и меньше', () => {
-    // expect(calculateBonus(20, 40)).toBeGreaterThan(40);
-    // expect(calculateBonus(30, 25)).toBeLessThan(55);
+    expect(calculateBonus(20, 40)).toBeGreaterThan(40);
+    expect(calculateBonus(30, 25)).toBeLessThan(55);
   });
 });
